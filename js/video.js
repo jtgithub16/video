af72c34272ebdd08a12c9fde09c7f6e3a4ca2845
johannghub <90212860+jtgithub16@users.noreply.github.com>
2023-11-08 09:23:45 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -34,19 +34,19 @@ document.querySelector("#slower").addEventListener("click", function() {
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate = video.playbackRate * 1.1;
+	video.playbackRate /= 0.9
 	console.log("Video Playback Rate" + video.playbackRate)
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	if(video.duration - video.currentTime < 10){
-		video.currentTime = 0;
-	}
-	else{
+	if(video.currentTime + 10 < video.duration){
 		video.currentTime += 10;
 	}
+	else{
+		video.currentTime = 0;
+	}
 	
-	console.log("Current Location" + video.currentTime)
+	console.log("Current Location" + video.currentTime.toFixed(1))
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
@@ -56,17 +56,17 @@ document.querySelector("#mute").addEventListener("click", function() {
 });	
 
 document.querySelector("#vintage").addEventListener("click", function() {
-	video.classList.remove('oldSchool')
+	video.classList.add('oldSchool')
 });
 
 document.querySelector("#orig").addEventListener("click", function() {
-	video.classList.add('oldSchool')
+	video.classList.remove('oldSchool')
 });	
 
 document.querySelector("#slider").addEventListener("input", function() { //slider update
 	video.volume = this.value/100;
 	console.log(this.value)		
-	document.querySelector("#volume").textContent = (this.value) + "%";
+	document.querySelector("#volume").textContent = (this.valuex	) + "%";
 
 
 })
